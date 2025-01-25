352a4aba94f015fc2f5586bd39bebe77085caccd
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-01-25 10:56:29 +0530

    Update tools.ts

--- lib/tools.ts
+++ lib/tools.ts
@@ -1,8 +1,8 @@
-import { Palette, Sliders, Type, Crop } from "lucide-react";
+import { Palette, Sliders, Type } from "lucide-react";
 
 export const tools = [
   // { id: 'frames', icon: Frame, label: 'Frames' },
-  { id: "crop", icon: Crop, label: "Crop" },
+  // { id: "crop", icon: Crop, label: "Crop" },
   { id: "filters", icon: Palette, label: "Filters" },
   { id: "tweaks", icon: Sliders, label: "Tweaks" },
   // { id: 'stickers', icon: Sticker, label: 'Stickers' },
